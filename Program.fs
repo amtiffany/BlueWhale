98ccfa402097e8ccfb9c2dbe39033a8e7acbc509
alice <alice@pauls-MacBook-Pro.local>
2022-03-07 16:13:18 -0800

    printing list

--- Program.fs
+++ Program.fs
@@ -2,6 +2,9 @@
 
 open System
 
+let lst = 1::2::3::[]
+printfn "%A" lst
+
 // Define a function to construct a message to print
 let from whom =
     sprintf "from %s" whom
@@ -11,3 +14,4 @@ let main argv =
     let message = from "F#" // Call the function
     printfn "Hello world %s" message
     0 // return an integer exit code
+
